32e017c424ac953d86257b532c70c4aa35c9c5f0
Pete Fairhurst <markedup@users.noreply.github.com>
2023-07-19 07:51:32 +0000

    Completed the course

---
+++ src/main/java/bank/Account.java
@@ -0,0 +1,64 @@
+package bank;
+
+import bank.exceptions.AmountException;
+
+public class Account {
+  private int id;
+  private String type;
+  private double balance;
+
+  public Account(int id, String type, double balance) {
+    setId(id);
+    setType(type);
+    setBalance(balance);
+  }
+
+  public int getId() {
+    return this.id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public double getBalance() {
+    return this.balance;
+  }
+
+  public void setBalance(double balance) {
+    this.balance = balance;
+  }
+
+  public void deposit(double amount) throws AmountException {
+    if (amount < 1) {
+      throw new AmountException("The minimum deposit amount is 1");
+    } else {
+      double newBalance = balance + amount;
+      setBalance(newBalance);
+      DataSource.updateAccountBalance(id, newBalance);
+    }
+  }
+
+  public void withdraw(double amount) throws AmountException {
+
+    if (amount < 0) {
+      throw new AmountException(("The withdrawl amount must be greater than 0."));
+    } else if (amount > getBalance()) {
+      throw new AmountException("You do not have sufficient funds to withdraw this amount.");
+    } else {
+      double newBalance = balance - amount;
+      setBalance(newBalance);
+      DataSource.updateAccountBalance(id, newBalance);
+    }
+
+  }
+
+}
